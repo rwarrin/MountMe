930c74c9898d0ab5c951828e4c9af5f7793b8ec6
Rick Warrington <phial1020@gmail.com>
2011-01-07 02:22:59 -0700

    Major logic refactoring.  Added new SummonMount function
    Signed-off-by: Rick Warrington <phial1020@gmail.com>

--- core.lua
+++ core.lua
@@ -11,12 +11,12 @@ local function GetMountCount(mountlist)
 	return counter;
 end
 
-local function SummonGroundMount(numberofgroundmounts)
-	CastSpellByName(groundmounts[ChooseMount(numberofgroundmounts)][1]);
-end
-
-local function SummonFlyingMount(numberofflyingmounts)
-	CastSpellByName(flyingmounts[ChooseMount(numberofflyingmounts)][1]);
+local function SummonMount(mounttype, mountcount)
+	if(mounttype == "FLYING") then
+		CastSpellByName(flyingmounts[ChooseMount(mountcount)][1]);	
+	else -- if(mounttype == "GROUND") then
+		CastSpellByName(groundmounts[ChooseMount(mountcount)][1]);
+	end
 end
 
 function RandomMount() 
@@ -30,43 +30,56 @@ function RandomMount()
 		Dismount();
 	end
 	
+	-- Check to see if the zone is not flyable
+	if(isflyablezone == 0) then
+		SummonMount("GROUND", numberofgroundmounts);
+	end
+		
 	-- Check if the player is in Vashj'ir
 	if(currentzonename == "Abyssal Depths" or currentzonename == "Shimmering Expanse" or currentzonename == "Kelp'thar Forrest") then
 		if(IsSwimming()) then
 			CastSpellByName("Abyssal Seahorse");
 		end
 	end
-		
+	
+	-- Check to see if the zone is a Battleground
+	if(currentcontinent == -1) then
+		SummonMount("GROUND", numberofgroundmounts);
+	end
+	
+	-- Check to see if the player can fly or not
 	if(CHARACTER_CAN_FLY == true) then
-		if(isflyablezone == 1) then
-			if(currentcontinent == -1) then -- Battleground
-				SummonGroundMount(numberofgroundmounts);
-			elseif(currentcontinent == 1 or currentcontinent == 2 or currentcontinent == 5) then -- Kalimdor or Eastern Kingdoms or Deepholm
-				if(SKILL_OLDWORLD_FLYING == true) then
-					SummonFlyingMount(numberofflyingmounts);
-				else
-					SummonGroundMount(numberofgroundmounts);
-				end
-			elseif(currentcontinent == 3) then -- Outlands
-					SummonFlyingMount(numberofflyingmounts);
-			elseif(currentcontinent == 4) then -- Northrend
-				if(SKILL_NORTHREND_FLYING == true) then
-					SummonFlyingMount(numberofflyingmounts);
-				else
-					SummonGroundMount(numberofgroundmounts);
-				end
-			end
+		-- Check if the player is in Kalimdor
+		if(currentcontinent == 1 and SKILL_OLDWORLD_FLYING == true) then
+			SummonMount("FLYING", numberofflyingmounts)
+		
+		-- Check if the player is in Eastern Kingdoms
+		elseif(currentcontinent == 2 and SKILL_OLDWORLD_FLYING == true) then
+			SummonMount("FLYING", numberofflyingmounts);
+					
+		-- Check if the player is in Deepholm
+		elseif(currentcontinent == 5 and SKILL_OLDWORLD_FLYING == true) then
+			SummonMount("FLYING", numberofflyingmounts)
+			
+		-- Check if the player is in Outlands
+		elseif(currentcontinent == 3) then
+			SummonMount("FLYING", numberofflyingmounts)
+		
+		-- Check if the player is in Northrend
+		elseif(currentcontinent == 4) then
+			SummonMount("FLYING", numberofflyingmounts)
+		
+		-- Player can't fly anywhere
 		else
-			SummonGroundMount(numberofgroundmounts);
-		end	
+			SummonMount("GROUND", numberofgroundmounts);
+		end		
 	else
-		SummonGroundMount(numberofgroundmounts);
+		SummonMount("GROUND", numberofgroundmounts);
 	end
 end
 
 -- Create frame for addon and register for events
 local MountMe = CreateFrame("FRAME", "MountMe_Frame", UIParent);
-DEFAULT_CHAT_FRAME:AddMessage("MountMe Loaded.");
 
 -- Create slash command
 SLASH_MOUNTME1 = "/mountme";
